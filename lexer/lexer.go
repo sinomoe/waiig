30a24e467c6aa0bd8dba49976f7680154034a447
sinomoe <sino2322@gmail.com>
2023-06-12 20:40:56 +0800

    feat: lexer 1

---
+++ lexer/lexer.go
@@ -0,0 +1,105 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int  // 所输入字符串中的当前位置(指向当前字符)
+	readPosition int  // 所输入字符串中的当前读取位置(指向当前字符之后的一个字符)
+	ch           byte // 当前正在查看的字符
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar 读取 input 中的下一个字符
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	// 跳过所有空白字符
+	l.skipWhitespace()
+
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			// 识别标识符和关键字
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal) // 可能是关键字或者标识符
+			return tok                                // readIdentifier 中已经将 pos 移动到了下一位，这里必须提前返回
+		} else if isDigit(l.ch) {
+			// 识别数字字面量
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
---
+++ token/token.go
@@ -0,0 +1,45 @@
+package token
+
+type TokenType string // 使用 string 为了方便调试，会有性能损耗，实际情况使用其他类型
+
+type Token struct {
+	Type    TokenType // 词元类型
+	Literal string    // 字面量
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // 未知的词法单元或字符
+	EOF     = "EOF"     // 文件结尾
+
+	// 标识符+字面量
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// 运算符
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
