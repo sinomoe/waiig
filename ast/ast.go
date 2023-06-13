74e08689a930f51c4d17fdf9b101d17b563d1620
sinomoe <sino2322@gmail.com>
2023-06-13 13:58:24 +0800

    feat: parser 1

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "monkey/token"
+
+// Node AST 中的每个节 点都必须实现 Node 接口，也就是说必须提供 TokenLiteral()方法，该方法返回与其 关联的词法单元的字面量
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // 仅占位，防止 Statement 和 Expression 混淆
+}
+
+type Expression interface {
+	Node
+	expressionNode() // 仅占位，防止 Statement 和 Expression 混淆
+}
+
+// Program 节点是语法分析器生成的每个 AST 的根节点
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement let 语句节点
+type LetStatement struct {
+	Token token.Token // token.LET 词法单元
+	Name  *Identifier // 左侧标识符
+	Value Expression  // 右侧表达式、字面量
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT 词法单元
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,102 @@
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+// Parser 是语法解析器，负责将词法单元解析为 AST
+type Parser struct {
+	l         *lexer.Lexer
+	errors    []string
+	curToken  token.Token // 输入中的当前词法单元
+	peekToken token.Token // 下一个词法单元
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+	// 读取两个词法单元，以设置curToken和peekToken
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{} // 构建 AST 根节点
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF { // 循环将 Token 读完
+		stmt := p.parseStatement() // Program 由语句组成，循环解析语句
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+// parseStatement 解析语句
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// parseLetStatement 解析 let 语句
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken} // 初始化 let 语句节点
+	// let 语句前两个 token 一定是 IDENT 和 ASSIGN
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// TODO: 跳过对表达式的处理，直到遇见分号
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// peekError 向 l.errors 中追加错误信息
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
