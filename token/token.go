bf93861b9e6953f5a1f2bfb4ec2e14ed066e0add
sinomoe <sino2322@gmail.com>
2023-06-16 13:06:47 +0800

    feat: use byte as token type underlying type

--- token/token.go
+++ token/token.go
@@ -1,6 +1,40 @@
 package token
 
-type TokenType string // 使用 string 为了方便调试，会有性能损耗，实际情况使用其他类型
+type TokenType byte
+
+func (t TokenType) String() string {
+	return tokenTypeStringMap[t]
+}
+
+var tokenTypeStringMap = map[TokenType]string{
+	ILLEGAL:   "ILLEGAL",
+	EOF:       "EOF",
+	IDENT:     "IDENT",
+	INT:       "INT",
+	ASSIGN:    "=",
+	PLUS:      "+",
+	MINUS:     "-",
+	BANG:      "!",
+	ASTERISK:  "*",
+	SLASH:     "/",
+	LT:        "<",
+	GT:        ">",
+	EQ:        "==",
+	NOT_EQ:    "!=",
+	COMMA:     ",",
+	SEMICOLON: ";",
+	LPAREN:    "(",
+	RPAREN:    ")",
+	LBRACE:    "{",
+	RBRACE:    "}",
+	FUNCTION:  "FUNCTION",
+	LET:       "LET",
+	TRUE:      "TRUE",
+	FALSE:     "FALSE",
+	IF:        "IF",
+	ELSE:      "ELSE",
+	RETURN:    "RETURN",
+}
 
 type Token struct {
 	Type    TokenType // 词元类型
@@ -8,41 +42,41 @@ type Token struct {
 }
 
 const (
-	ILLEGAL = "ILLEGAL" // 未知的词法单元或字符
-	EOF     = "EOF"     // 文件结尾
+	ILLEGAL TokenType = iota // 未知的词法单元或字符
+	EOF                      // 文件结尾
 
 	// 标识符+字面量
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 1343456
+	IDENT // add, foobar, x, y, ...
+	INT   // 1343456
 
 	// 运算符
-	ASSIGN   = "="
-	PLUS     = "+"
-	MINUS    = "-"
-	BANG     = "!"
-	ASTERISK = "*"
-	SLASH    = "/"
-	LT       = "<"
-	GT       = ">"
-	EQ       = "=="
-	NOT_EQ   = "!="
+	ASSIGN
+	PLUS
+	MINUS
+	BANG
+	ASTERISK
+	SLASH
+	LT
+	GT
+	EQ
+	NOT_EQ
 
 	// 分隔符
-	COMMA     = ","
-	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
+	COMMA
+	SEMICOLON
+	LPAREN
+	RPAREN
+	LBRACE
+	RBRACE
 
 	// 关键字
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
-	TRUE     = "TRUE"
-	FALSE    = "FALSE"
-	IF       = "IF"
-	ELSE     = "ELSE"
-	RETURN   = "RETURN"
+	FUNCTION
+	LET
+	TRUE
+	FALSE
+	IF
+	ELSE
+	RETURN
 )
 
 var keywords = map[string]TokenType{
